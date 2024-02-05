b9de68c2a0fe7f110eb32736dfe381b824dff003
“Zümra <zumrasoylen@yahoo.com>
2024-02-05 19:50:03 +0300

    completed exercise 2.19

--- part2/countries/src/components/Countries.jsx
+++ part2/countries/src/components/Countries.jsx
@@ -1,33 +1,58 @@
+import { useState } from "react";
+
+const Country = ({ country }) => {
+  return (
+    <div>
+      <h1>{country.name.common}</h1>
+      <div>capital {country.capital}</div>
+      <div>area {country.area}</div>
+      <h3>languages:</h3>
+      <ul>
+        {Object.values(country.languages).map((lan) => {
+          return <li key={lan}>{lan}</li>;
+        })}
+      </ul>
+      <img src={country.flags.png} alt={country.flags.alt} width="200" />
+    </div>
+  );
+};
+
 const Countries = ({ countries }) => {
+  const [viewCountry, setViewCountry] = useState(null);
   const len = countries.length;
   let content = null;
 
+  const showCountry = (country) => {
+    setViewCountry(country);
+  };
   if (len > 10) {
     content = <div>Too many matches, specify another filter</div>;
+  } else if (viewCountry != null) {
+    content = <Country country={viewCountry} />;
   } else if (len > 1) {
     content = (
       <ul>
         {countries.map((country) => {
-          return <li key={country.name.common}>{country.name.common}</li>;
+          return (
+            <div>
+              <li key={country.name.common}>
+                {country.name.common}
+                <button
+                  onClick={() => {
+                    showCountry(country);
+                  }}
+                >
+                  show
+                </button>
+              </li>
+            </div>
+          );
         })}
       </ul>
     );
   } else if (len === 1) {
     const country = countries[0];
-    content = (
-      <div>
-        <h1>{country.name.common}</h1>
-        <div>capital {country.capital}</div>
-        <div>area {country.area}</div>
-        <h3>languages:</h3>
-        <ul>
-          {Object.values(country.languages).map((lan) => {
-            return <li key={lan}>{lan}</li>;
-          })}
-        </ul>
-        <img src={country.flags.png} alt={country.flags.alt} width="200"/>
-      </div>
-    );
+    content = <Country country={country} />;
   } else {
     content = <div>No country matched with the searched text</div>;
   }
