6e37b1f30c95728f9b38a6a0b69aab0400a67f11
“Zümra <zumrasoylen@yahoo.com>
2024-02-09 22:22:31 +0300

    completed exercises 3.3 and 3.4

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const data = require("./data.json");
+let data = require("./data.json");
 
 app.use(express.json());
 
@@ -8,11 +8,27 @@ app.get("/api/persons", (request, response) => {
   response.json(data);
 });
 
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const person = data.find((person) => person.id === id);
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  data = data.filter((data) => data.id != id);
+  response.status(204).end();
+});
+
 app.get("/info", (request, response) => {
-  const date = Date()
-  const content =  `<div><p>Phonebook has info for ${data.length} people</p> <p>${date}</p></div>`
-  response.send(content)
-})
+  const date = Date();
+  const content = `<div><p>Phonebook has info for ${data.length} people</p> <p>${date}</p></div>`;
+  response.send(content);
+});
 
 const PORT = 3001;
 app.listen(PORT, () => {
